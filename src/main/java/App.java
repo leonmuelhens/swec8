55c4464526b3521ec5d1c776b0f3c26ade35d928
lhrb <lukas.block@protonmail.com>
2018-10-25 23:23:23 +0200

    Add plugin: eclipse, idea Add dependecy lwjgl Change repository to mavenCentral

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,7 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    	App app = new App();
+        System.out.println(app.getGreeting());
     }
 }
