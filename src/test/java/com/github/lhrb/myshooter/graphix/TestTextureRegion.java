9315eeb65f38c985415cfbce8977e7fa283131c8
lhrb <lukas.block@protonmail.com>
2018-11-07 20:28:47 +0100

    Add TextureRegion test class

---
+++ src/test/java/com/github/lhrb/myshooter/graphix/TestTextureRegion.java
@@ -0,0 +1,144 @@
+/**
+ * 
+ */
+package com.github.lhrb.myshooter.graphix;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author exa
+ *
+ */
+public class TestTextureRegion {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test method for {@link com.github.lhrb.myshooter.graphix.TextureRegion#TextureRegion(com.github.lhrb.myshooter.graphix.Texture)}.
+     */
+    @Test
+    public void testTextureRegionTexture() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.github.lhrb.myshooter.graphix.TextureRegion#TextureRegion(com.github.lhrb.myshooter.graphix.Texture, int, int, int, int)}.
+     */
+    @Test
+    public void testTextureRegionTextureIntIntIntInt() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.github.lhrb.myshooter.graphix.TextureRegion#getTexture()}.
+     */
+    @Test
+    public void testGetTexture() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.github.lhrb.myshooter.graphix.TextureRegion#setTexture(com.github.lhrb.myshooter.graphix.Texture)}.
+     */
+    @Test
+    public void testSetTexture() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.github.lhrb.myshooter.graphix.TextureRegion#getOriginX()}.
+     */
+    @Test
+    public void testGetOriginX() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.github.lhrb.myshooter.graphix.TextureRegion#setOriginX(int)}.
+     */
+    @Test
+    public void testSetOriginX() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.github.lhrb.myshooter.graphix.TextureRegion#getOriginY()}.
+     */
+    @Test
+    public void testGetOriginY() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.github.lhrb.myshooter.graphix.TextureRegion#setOriginY(int)}.
+     */
+    @Test
+    public void testSetOriginY() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.github.lhrb.myshooter.graphix.TextureRegion#getRegionWidth()}.
+     */
+    @Test
+    public void testGetRegionWidth() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.github.lhrb.myshooter.graphix.TextureRegion#setRegionWidth(int)}.
+     */
+    @Test
+    public void testSetRegionWidth() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.github.lhrb.myshooter.graphix.TextureRegion#getRegionHeight()}.
+     */
+    @Test
+    public void testGetRegionHeight() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.github.lhrb.myshooter.graphix.TextureRegion#setRegionHeight(int)}.
+     */
+    @Test
+    public void testSetRegionHeight() {
+        fail("Not yet implemented");
+    }
+
+}
