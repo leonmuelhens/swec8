55aeebd59b216c3642d1d67edab78cc4e94e5e49
lhrb <lukas.block@protonmail.com>
2018-11-27 13:37:40 +0100

    Add sample animation

--- core/src/com/github/lhrb/nemo/TestStage.java
+++ core/src/com/github/lhrb/nemo/TestStage.java
@@ -6,23 +6,20 @@ package com.github.lhrb.nemo;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.github.lhrb.nemo.actors.ActorPrefab;
 import com.github.lhrb.nemo.actors.Player;
 
 /**
  * @author exa
  *
- *just add new LwjglApplication(new TestStage(), "Killing Nemo", 800, 600);
+ *just add new LwjglApplication(new TestStage(), "Killing Nemo", 1200, 600);
  * @com.github.lhrb.nemo.desktop.DesktopLauncher 
  */
 public class TestStage extends Game {
     
 
-    private Player player;
     private Stage stage;
 
     /* (non-Javadoc)
@@ -37,8 +34,10 @@ public class TestStage extends Game {
     }
     
     private void init() {
-        player = new Player(20,20, stage);
-
+        Player player = new Player(20,20, stage);
+        ActorPrefab explosion = new ActorPrefab(200,200,stage);
+        explosion.setAnimation(explosion.loadAnimation(
+                               "explosion.png", 6, 6, 0.05f, true));
        
     }
     
@@ -54,9 +53,7 @@ public class TestStage extends Game {
         
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-       
-        
-        
+               
         stage.draw();
         
     }
