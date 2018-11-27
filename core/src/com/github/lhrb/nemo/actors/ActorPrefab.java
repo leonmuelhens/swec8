5f365e439a33faf125a9edb353dca461ff19f70f
lhrb <lukas.block@protonmail.com>
2018-11-27 23:22:26 +0100

    Update outsource animation loader code to increase cohesion

--- core/src/com/github/lhrb/nemo/actors/ActorPrefab.java
+++ core/src/com/github/lhrb/nemo/actors/ActorPrefab.java
@@ -3,10 +3,7 @@
  */
 package com.github.lhrb.nemo.actors;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -15,7 +12,6 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.Array;
 
 /**
  * @author exa
@@ -75,67 +71,7 @@ public class ActorPrefab extends Group {
         setSize(width, height);
         setOrigin(width/2, height/2);
     }
-    
-    
-    /**
-     * Returns an animation based on a sequence inside a 
-     * image file
-     * 
-     * This method is actually a canidate for an static method in
-     * another class though
-     * 
-     * be aware this method does not provide any security mechanism
-     * pass an empty filename or null or a zero value as row/col
-     * will cause an error
-     * 
-     * @param fileName
-     * @param rows
-     * @param cols
-     * @param frameDuration
-     * @param loop
-     * @return
-     */
-    public Animation<TextureRegion> loadAnimation(String fileName, 
-                                                  int rows, int cols, 
-                                                  float frameDuration, boolean loop){
         
-        Texture texture = new Texture(Gdx.files.internal(fileName), true);
-        texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-        int tileWidth = texture.getWidth() / cols;
-        int tileHeight = texture.getHeight() / rows;
-        TextureRegion[][] tmp = TextureRegion.split(texture, tileWidth, tileHeight);
-        
-        Array<TextureRegion> textureArray = new Array<TextureRegion>();
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                textureArray.add(tmp[i][j]);
-            }
-        }
-        
-        Animation<TextureRegion> tmpAnimation = new Animation<TextureRegion>
-                                                (frameDuration, textureArray);
-        
-        if(loop) {
-            tmpAnimation.setPlayMode(Animation.PlayMode.LOOP);
-        }else {
-            tmpAnimation.setPlayMode(Animation.PlayMode.NORMAL);
-        }
-        
-        return tmpAnimation;
-    }
-    
-    /**
-     * Simple helper method 
-     * for game objects that may be respresentetd
-     * by a single image
-     * 
-     * @param fileName
-     * @return
-     */
-    public Animation<TextureRegion> loadTexture(String fileName){
-        return loadAnimation(fileName, 1, 1, 1, true);
-    }
-    
    
     public boolean isAnimationFinished() {
         return animation.isAnimationFinished(elapsedTime);
