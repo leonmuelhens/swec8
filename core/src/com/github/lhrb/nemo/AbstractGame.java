a9e5455291d66406c2a6955a1a06ba63e9cc5e78
lhrb <lukas.block@protonmail.com>
2018-11-28 08:54:07 +0100

    Add abstract game class which supports screen polymorphism

---
+++ core/src/com/github/lhrb/nemo/AbstractGame.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.github.lhrb.nemo;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.github.lhrb.nemo.screen.AbstractScreen;
+
+/**
+ * @author exa
+ * abtract class to simplify creating different "Games" mostly for debug puposes
+ * 
+ */
+public abstract class AbstractGame extends Game {
+    
+    //defined as static to simplify usage
+    private static AbstractGame game;
+    
+    
+    public AbstractGame() {
+        game = this;
+    }
+
+    /* (non-Javadoc)
+     * @see com.badlogic.gdx.ApplicationListener#create()
+     */
+    @Override
+    public void create() {
+        InputMultiplexer iMultiplexer = new InputMultiplexer();
+        Gdx.input.setInputProcessor(iMultiplexer);
+    }
+    
+    
+    
+    public static void setActiveScreen(AbstractScreen screen) {
+        if(screen == null) return;
+        game.setScreen(screen);
+    }
+
+}
--- core/src/com/github/lhrb/nemo/KillingNemo.java
+++ core/src/com/github/lhrb/nemo/KillingNemo.java
@@ -1,33 +1,11 @@
 package com.github.lhrb.nemo;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class KillingNemo extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class KillingNemo extends AbstractGame {
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture(Gdx.files.internal("player_laser.png"));
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	    super.create();
+	    setActiveScreen(null);
 	}
 }
