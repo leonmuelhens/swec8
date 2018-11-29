027700a8721bc6a925e5769879a9cf74a20f881c
Thorsten1 <topsecret.thorsten@googlemail.com>
2018-11-29 15:49:12 +0100

    Bilder und so

--- core/src/com/github/lhrb/nemo/actors/EnemyTwo.java
+++ core/src/com/github/lhrb/nemo/actors/EnemyTwo.java
@@ -14,7 +14,7 @@ public class EnemyTwo extends ActorPrefab {
 
         public EnemyTwo(float x, float y, Stage stage) {
             super(x,y,stage);
-            setAnimation(AnimationLoader.loadTexture("Gegner1.png"));
+            setAnimation(AnimationLoader.loadTexture("Gegner2.png"));
             setRotation(180);
             setAcceleration(1000000);
             setSpeedMax(300);
