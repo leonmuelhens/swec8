171b73dc97c34fa4dd94df5eaaf54d6f86980eba
Thorsten1 <topsecret.thorsten@googlemail.com>
2018-11-29 16:00:39 +0100

    Final state before Feature implementation for enemys (requires shooting and collisions)

--- core/src/com/github/lhrb/nemo/screen/FirstLevelScreen.java
+++ core/src/com/github/lhrb/nemo/screen/FirstLevelScreen.java
@@ -9,9 +9,7 @@ public class FirstLevelScreen extends AbstractScreen{
     public void init() {
         Background bg = new Background(0,0,gameStage,1);
         Background bg2 = new Background(0,1200,gameStage,1);
-        EnemyOne enemie = new EnemyOne(500,500,gameStage);
-        EnemyTwo enemy2 = new EnemyTwo(600,500,gameStage);
-        EnemyThree enemy3 = new EnemyThree(700,500,gameStage);
+        EnemyOne enemie = new EnemyOne(300,500,gameStage);
         Player player = new Player(20,20, gameStage);
         // player.setWorldDimension(1200, 600); // should get reworked
         ActorPrefab explosion = new ActorPrefab(200,200, gameStage);
