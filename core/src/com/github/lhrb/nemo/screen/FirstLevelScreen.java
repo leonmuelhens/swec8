4439cada7a155b2840a8cbbc267dbbed3ac415e3
Christoph <Christoph@CHRISTOPH-PC>
2018-11-30 15:16:11 +0100

    Change firstLevel to stop spawning after 3 * 60 seconds

--- core/src/com/github/lhrb/nemo/screen/FirstLevelScreen.java
+++ core/src/com/github/lhrb/nemo/screen/FirstLevelScreen.java
@@ -6,12 +6,12 @@ import com.github.lhrb.nemo.util.AnimationLoader;
 
 public class FirstLevelScreen extends AbstractScreen{
     EnemyFactory factory = new EnemyFactory(1,gameStage);
-
+    float gameTime;
     Player player;
-    ActorPrefab explosion;
 
     @Override
     public void init() {
+        gameTime = 0F;
         Background bg = new Background(0,0,gameStage,1);
         Background bg2 = new Background(0,1200,gameStage,1);
 
@@ -21,7 +21,18 @@ public class FirstLevelScreen extends AbstractScreen{
     @Override
     public void update(float delta) {
         // TODO Auto-generated method stub
-        factory.continueManufacture(delta);
+        gameTime += delta;
+
+        /* Once we define an abstract class for gameScreens, we can define a variable
+           for how long the level shall take and replace the hardcorded 3*6
+         */
+
+        if (gameTime < 3*60) {
+            factory.continueManufacture(delta);
+        } else {
+            // we have to initialize the bossScreen
+            System.out.println("The Boss level should start here");
+        }
     }
 
 }
