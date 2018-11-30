461ae4de77d2370454feb23488957b255ff85516
lhrb <lukas.block@protonmail.com>
2018-11-30 12:09:06 +0100

    Add example collision response

--- core/src/com/github/lhrb/nemo/screen/FirstLevelScreen.java
+++ core/src/com/github/lhrb/nemo/screen/FirstLevelScreen.java
@@ -6,17 +6,23 @@ import com.github.lhrb.nemo.actors.Background;
 import com.github.lhrb.nemo.util.AnimationLoader;
 
 public class FirstLevelScreen extends AbstractScreen{
+    
+    Player player;
+    ActorPrefab explosion;
 
     @Override
     public void init() {
         Background bg = new Background(0,0,gameStage,1);
         Background bg2 = new Background(0,1200,gameStage,1);
 
-        Player player = new Player(20,20, gameStage);
+        player = new Player(20,20, gameStage);
+        player.setShapePolygon(8);
+        
         // player.setWorldDimension(1200, 600); // should get reworked
-        ActorPrefab explosion = new ActorPrefab(200,200, gameStage);
+        explosion = new ActorPrefab(200,200, gameStage);
         explosion.setAnimation(AnimationLoader.loadAnimation(
                                "explosion.png", 6, 6, 0.05f, true));
+        explosion.setShapePolygon(8);
 
         
     }
@@ -24,7 +30,9 @@ public class FirstLevelScreen extends AbstractScreen{
     @Override
     public void update(float delta) {
         // TODO Auto-generated method stub
-        
+        if(player.overlap(explosion)) {
+            System.out.println("Collision " + delta);
+        }
     }
 
 }
