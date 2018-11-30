2afc9a9f5aaef7bb1745a5c31ee41a1b81cc4324
Christoph <Christoph@CHRISTOPH-PC>
2018-11-30 14:10:47 +0100

    remove not required testing animations

--- core/src/com/github/lhrb/nemo/screen/FirstLevelScreen.java
+++ core/src/com/github/lhrb/nemo/screen/FirstLevelScreen.java
@@ -11,15 +11,8 @@ public class FirstLevelScreen extends AbstractScreen{
     public void init() {
         Background bg = new Background(0,0,gameStage,1);
         Background bg2 = new Background(0,1200,gameStage,1);
-        EnemyOne enemy = new EnemyOne(300,500,gameStage);
-        EnemyTwo enemy2 = new EnemyTwo(100,500,gameStage);
-        EnemyThree enemy3 = new EnemyThree(500,500,gameStage);
 
         Player player = new Player(20,20, gameStage);
-        // player.setWorldDimension(1200, 600); // should get reworked
-        ActorPrefab explosion = new ActorPrefab(200,200, gameStage);
-        explosion.setAnimation(AnimationLoader.loadAnimation(
-                               "explosion.png", 6, 6, 0.05f, true));
 
         
     }
@@ -28,7 +21,6 @@ public class FirstLevelScreen extends AbstractScreen{
     public void update(float delta) {
         // TODO Auto-generated method stub
         factory.continueManufacture(delta);
-        //factory.modifySpawnRate();
     }
 
 }
