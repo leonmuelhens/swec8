1185f801f001b72c15700d881d4a0392e44507ae
Christoph Waninger <christoph@cwsbit.de>
2018-12-02 21:07:55 +0100

    balance the enemy weapons so that user can actually try to win

--- core/src/com/github/lhrb/nemo/actors/weapons/WeaponSpread.java
+++ core/src/com/github/lhrb/nemo/actors/weapons/WeaponSpread.java
@@ -5,20 +5,28 @@ import com.github.lhrb.nemo.actors.shots.SingleShot;
 import com.github.lhrb.nemo.util.SoundManager;
 
 public class WeaponSpread extends Weapon {
+    private float angleModifier;
+
     public WeaponSpread(Stage stage) {
         super(stage, 0.6f);
+        angleModifier = 45;
     }
 
     public WeaponSpread(Stage stage, float cooldown) {
         super(stage, cooldown);
     }
 
+    public WeaponSpread(Stage stage, float cooldown,float angleModifier) {
+        super(stage, cooldown);
+        this.angleModifier = angleModifier;
+    }
+
     @Override
     public void fire(float x, float y, float angle) {
         if (isReady()) {
-            new SingleShot(x, y, getStage(), angle-45);
+            new SingleShot(x, y, getStage(), angle - angleModifier);
             new SingleShot(x, y, getStage(), angle);
-            new SingleShot(x, y, getStage(), angle+45);
+            new SingleShot(x, y, getStage(), angle + angleModifier);
             resetCooldownTimer();
 
             SoundManager.getInstance().playSound("laser");
