0ef99fc0a255e90e3fde1e8dedbc735b53a863cb
Christoph Waninger <christoph@cwsbit.de>
2018-12-03 11:03:09 +0100

    use big button in main menu

--- core/src/com/github/lhrb/nemo/screen/MainMenuScreen.java
+++ core/src/com/github/lhrb/nemo/screen/MainMenuScreen.java
@@ -26,7 +26,7 @@ public class MainMenuScreen extends AbstractScreen {
         ActorPrefab bg = new ActorPrefab(0,0, gameStage);
         bg.setAnimation(AnimationLoader.loadTexture("mainmenu_background.png"));
         
-        TextButton startBtn = new TextButton("Neues Spiel", GuiManager.getInstance().getTxtBtnStyle());
+        TextButton startBtn = new TextButton("Neues Spiel", GuiManager.getInstance().getTxtBtnStyleBig());
         startBtn.setPosition(200, 300);
         startBtn.addListener(
                 (Event e) ->{
