01c2c1afa433189618fc815a8302d1860ec1e975
leonmuelhens <leon.muelhens@alumni.fh-aachen.de>
2018-12-12 10:53:22 +0100

    Add Bomb does damage

--- core/src/com/github/lhrb/nemo/actors/shots/Bomb.java
+++ core/src/com/github/lhrb/nemo/actors/shots/Bomb.java
@@ -1,8 +1,10 @@
 package com.github.lhrb.nemo.actors.shots;
 
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.github.lhrb.nemo.actors.ActorPrefab;
 import com.github.lhrb.nemo.actors.PhysicalActor;
+import com.github.lhrb.nemo.actors.enemies.Enemy;
 import com.github.lhrb.nemo.actors.weapons.Weapon;
 import com.github.lhrb.nemo.util.AnimationLoader;
 import com.github.lhrb.nemo.util.SoundManager;
@@ -43,15 +45,38 @@ public class Bomb extends Shots {
 
         // Reliable explosion with margin of [-2;+2]
         if (getY() > explodeY-2 && getY() < explodeY+2) {
-            System.out.println("BOOM!");
             SoundManager.getInstance().playSound("explosion");
 
+            new Explosion(getX(),getY(),getStage());
             //code below is bad
-            new PhysicalActor(getX(), getY(), getStage())
-                    .setAnimation(AnimationLoader.get().animation(
-                            "explosion.png", 6, 6, 0.05f, false));
+            //new ActorPrefab(getX(), getY(), getStage())
+            //        .setAnimation(AnimationLoader.get().animation(
+            //                "big_explosion.png", 6, 6, 0.05f, false));
 
             remove();
+        }
+    }
+
+
+    class Explosion extends Shots {
+
+        public Explosion(float x, float y, Stage stage) {
+            super(x, y, stage, 0);
+
+            setAnimation(AnimationLoader.get().animation(
+                    "big_explosion.png", 6, 6, 0.05f, false));
+            setShapePolygon(8);
+            setPosition(getX()-(getWidth()/2), getY()-(getHeight()/2));
+
+            System.out.println("BOOM!");
+        }
+
+        @Override
+        public void act(float delta) {
+            super.act(delta);
+            if (isAnimationFinished()) {
+                remove();
+            }
         }
     }
 
