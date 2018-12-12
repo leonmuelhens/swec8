620d692338d75faa1435c2f2a69762e9f600d5ca
lhrb <lukas.block@protonmail.com>
2018-12-12 22:39:41 +0100

    Fix button left and right padding

--- core/src/com/github/lhrb/nemo/util/GuiManager.java
+++ core/src/com/github/lhrb/nemo/util/GuiManager.java
@@ -49,7 +49,7 @@ public class GuiManager {
     private TextButtonStyle createTextButtonStyle(int fontSize, Color color) {
         TextButtonStyle tmp = new TextButtonStyle();
         Texture   buttonTex   = new Texture( Gdx.files.internal("button.png") );
-        NinePatch buttonPatch = new NinePatch(buttonTex, 8,8,8,8);
+        NinePatch buttonPatch = new NinePatch(buttonTex, 10,10,8,8);
         tmp.up    = new NinePatchDrawable( buttonPatch );
         tmp.font      = createFont(fontSize);
         tmp.fontColor = Color.BLACK;
