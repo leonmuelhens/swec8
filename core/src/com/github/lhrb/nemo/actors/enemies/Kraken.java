c47d39c7dbd815505f6889a2d9f514c88760a00b
lhrb <lukas.block@protonmail.com>
2018-12-21 13:45:55 +0100

    Add exemplary tentacle attack

--- core/src/com/github/lhrb/nemo/actors/enemies/Kraken.java
+++ core/src/com/github/lhrb/nemo/actors/enemies/Kraken.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.github.lhrb.nemo.GameManager;
 import com.github.lhrb.nemo.KillingNemo;
 import com.github.lhrb.nemo.actors.MultiPartActor;
 import com.github.lhrb.nemo.actors.Section;
@@ -59,6 +60,7 @@ public class Kraken extends MultiPartActor {
         
         weapons = new ArrayList<Weapon>();
         weapons.add(new KrakenAttack(getStage()));
+        weapons.add(new TentacleAttack(getStage(), this));
         moveArea = new Vector3(50f, 550f, 425f);
     }
 
@@ -79,6 +81,36 @@ public class Kraken extends MultiPartActor {
                 addAction(Actions.removeActor());
                 //level over
                 KillingNemo.setActiveScreen(new LevelDoneScreen());
+            }
+        }
+    }
+    
+    public void resetPosition() {
+        for(Section s: getPartCollection()) {
+            switch(s.getID()) {
+            case 0:
+                s.setPosition(75, 50);
+                break;
+            case 1:
+                s.setPosition(30, 100);
+                break;
+            case 2:
+                s.setPosition(0, 50);
+                break;
+            case 3:
+                s.setPosition(60, 0);
+                break;
+            case 4:
+                s.setPosition(140, 0);
+                break;
+            case 5:
+                s.setPosition(170, 50);
+                break;
+            case 6:
+                s.setPosition(170, 100);
+                break;
+            default:
+                break;
             }
         }
     }
@@ -107,12 +139,93 @@ public class Kraken extends MultiPartActor {
         if (getElapsedTime() > 2f) {
             for (Weapon w: weapons) {
                 w.act(delta);
-                w.fire(getX()+108,getY()-(getHeight()/2),270);
+                w.fire(getX()+108,getY()+50,270);
             }
         }
     }
     
     
+    
+    
+    private class TentacleAttack extends Weapon{
+        
+        private Kraken parent;
+        private float elapsedTime = 0f;
+        
+        public TentacleAttack(Stage stage, Kraken parent) {
+            super(8f, stage);
+            this.parent = parent;
+            // TODO Auto-generated constructor stub
+        }
+
+        @Override
+        public void fire(float x, float y, float angle) {
+            // TODO Auto-generated method stub
+        }
+        
+        @Override
+        public void act(float delta){
+            if(!isReady()) {
+                cooldownTimer += delta;
+            }else {
+                elapsedTime += delta;
+                if(elapsedTime > 2f) {
+                    parent.resetPosition();
+                    resetCooldownTimer();
+                    elapsedTime = 0f;
+                }
+                else if(elapsedTime > 1f){
+                    for(Section s : parent.getPartCollection()) {
+                        if(s.getID() != 0) {
+                            s.setVisible(true);
+                        }
+                    }
+                }
+                else if(elapsedTime > 0.7f) {
+                    parent.getPartCollection().get(6).setVisible(true);
+                    parent.getPartCollection().get(6)
+                    .setPos(GameManager.get().getPlayerX(), 50f);
+                }
+                else if(elapsedTime > 0.6f) {
+                    parent.getPartCollection().get(5).setVisible(true);
+                    parent.getPartCollection().get(5)
+                    .setPos(GameManager.get().getPlayerX(), 125f);
+                }
+                else if(elapsedTime > 0.5f) {
+                    parent.getPartCollection().get(4).setVisible(true);
+                    parent.getPartCollection().get(4)
+                    .setPos(GameManager.get().getPlayerX(), 200f);
+                }
+                else if(elapsedTime > 0.4f) {
+                    parent.getPartCollection().get(3).setVisible(true);
+                    parent.getPartCollection().get(3)
+                    .setPos(GameManager.get().getPlayerX(), 275f);
+                }
+                else if(elapsedTime > 0.3f) {
+                    parent.getPartCollection().get(2).setVisible(true);
+                    parent.getPartCollection().get(2)
+                    .setPos(GameManager.get().getPlayerX(), 350f);
+                }
+                else if(elapsedTime > 0.2f) {
+                    parent.getPartCollection().get(1).setVisible(true);
+                    parent.getPartCollection().get(1)
+                          .setPos(GameManager.get().getPlayerX(), 400f);
+                    
+                }
+                else {
+                    for(Section s : parent.getPartCollection()) {
+                        if(s.getID() != 0) {
+                            s.setVisible(false);
+                        }
+                    }
+                }
+                
+            }
+            
+        }
+        
+        
+    }
     
     private class KrakenAttack extends Weapon{
 
@@ -121,7 +234,6 @@ public class Kraken extends MultiPartActor {
             // TODO Auto-generated constructor stub
         }
         
-
         @Override
         public void fire(float x, float y, float angle) {
             if(isReady()) {
@@ -132,10 +244,8 @@ public class Kraken extends MultiPartActor {
                 new KrakenShotS(x+50,y-60,angle,stage);
                 new KrakenShotS(x+125,y-10,angle,stage);
                 resetCooldownTimer();
-            }
-            
-        }
-        
+            }            
+        }        
     }
     
     
