0f3197904502457c24adb8797920cce098172e5d
Christoph Waninger <christoph@cwsbit.de>
2019-01-03 09:40:06 +0100

    add first attack (charge) to Shark deceleration/aceleration makes shark step into after chargeBack for some y-axis points -> may be fixed with finetuning, but it works and can be used like this as the shark needs to calm down after this attack ;)

--- core/src/com/github/lhrb/nemo/actors/enemies/Hai.java
+++ core/src/com/github/lhrb/nemo/actors/enemies/Hai.java
@@ -7,15 +7,9 @@ import java.util.ArrayList;
 
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.github.lhrb.nemo.KillingNemo;
 import com.github.lhrb.nemo.actors.MultiPartActor;
 import com.github.lhrb.nemo.actors.Section;
-import com.github.lhrb.nemo.actors.shots.Bomb;
-import com.github.lhrb.nemo.actors.shots.Torpedo;
 import com.github.lhrb.nemo.actors.weapons.Weapon;
-import com.github.lhrb.nemo.actors.weapons.WeaponSpread;
-import com.github.lhrb.nemo.screen.LevelScreen;
 import com.github.lhrb.nemo.util.AnimationLoader;
 
 
@@ -24,6 +18,7 @@ public class Hai extends MultiPartActor {
     private float moveAngle;
     private ArrayList<Weapon> weapons;
     private Vector3 moveArea;
+    private boolean chargeFront, chargeBack;
 
     public Hai(Stage stage){
         super(stage);
@@ -52,70 +47,67 @@ public class Hai extends MultiPartActor {
         setRotation(0);
         setAcceleration(1000);
         setSpeedMax(75);
-        setDeceleration(0);
+        setDeceleration(4000);
 
         weapons = new ArrayList<Weapon>();
 
         moveArea = new Vector3(50f, 550f, 410f);
+        chargeFront = false;
+        chargeBack = false;
 
     }
-
-    /* (non-Javadoc)
-     * @see com.github.lhrb.nemo.actors.MultiPartActor#handleCollision(com.github.lhrb.nemo.actors.Section)
-     */
-    @Override
-    public void handleCollision(Section section) {
-        //prevent middle part getting dmg
-        if(getPartSize() > 1 && section.getID() != 1) {
-            if(section.getDmg()) {
-                removePart(section);
-                section.perish();
-            }
-        }else if(getPartSize() == 1) {
-            if(section.getDmg()) {
-                removePart(section);
-                section.perish();
-                addAction(Actions.removeActor());
-                // first level over
-                // bad practice just for testing
-                try {
-                    ((LevelScreen)KillingNemo.getActiveScreen()).removeScreen();
-                }catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
 
 
     @Override
     public void act(float delta) {
         super.act(delta);
 
-        if (getY() > moveArea.z) {
-            moveAngle = 270;
-        }
-        else if (moveAngle == 270) {
-            moveAngle = 0;
-        }
-        else if (moveAngle == 0 && getX() > moveArea.y) {
-            moveAngle = 180;
-        }
-        else if (moveAngle == 180 && getX() < moveArea.x) {
-            moveAngle = 0;
+        if(chargeFront) {
+            if (getY() < 100f) {
+                chargeFront = false;
+                chargeBack = true;
+                moveAngle = 90;
+            }
+        } else if (chargeBack) {
+            if (getY() > moveArea.z) {
+                chargeBack = false;
+                setAcceleration(1000);
+                setSpeedMax(75);
+            }
+        }  else {
+            if (getY() > moveArea.z) {
+                moveAngle = 270;
+            }
+            else if (moveAngle == 270) {
+                moveAngle = 0;
+            }
+            else if (moveAngle == 0 && getX() > moveArea.y) {
+                moveAngle = 180;
+            }
+            else if (moveAngle == 180 && getX() < moveArea.x) {
+                moveAngle = 0;
+            }
+
+            if (getElapsedTime() > 2f) {
+                for (Weapon w: weapons) {
+                    w.act(delta);
+                    w.fire(getX()+108,getY()-(getHeight()/2),270);
+                }
+            }
+
+            if (getElapsedTime() > 2f && (int) getElapsedTime() % 10 == 0) {
+                chargeFront = true;
+
+                setAcceleration(5000);
+                setSpeedMax(800);
+                moveAngle = 270;
+            }
         }
 
         accelerationAtAngle(moveAngle);
 
         applyObjectPhysics(delta);
 
-        if (getElapsedTime() > 2f) {
-            for (Weapon w: weapons) {
-                w.act(delta);
-                w.fire(getX()+108,getY()-(getHeight()/2),270);
-            }
-        }
     }
 
 }
