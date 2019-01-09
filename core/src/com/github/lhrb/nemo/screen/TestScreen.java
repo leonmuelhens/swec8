effc3056c347f5309702fb209f7f377feb57a6c0
leonmuelhens <leon.muelhens@alumni.fh-aachen.de>
2019-01-09 20:03:58 +0100

    Fix readded test screen

---
+++ core/src/com/github/lhrb/nemo/screen/TestScreen.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.github.lhrb.nemo.screen;
+
+
+import com.github.lhrb.nemo.actors.CollisionManager;
+import com.github.lhrb.nemo.actors.Player;
+import com.github.lhrb.nemo.actors.enemies.Kraken;
+import com.github.lhrb.nemo.util.SoundManager;
+
+/**
+ * @author exa
+ *
+ */
+public class TestScreen extends AbstractScreen {
+    
+    /* (non-Javadoc)
+     * @see com.github.lhrb.nemo.screen.AbstractScreen#init()
+     */
+    @Override
+    public void init() {
+        SoundManager.getInstance().playTrack("boss");
+        
+        Kraken krake = new Kraken(300,600, gameStage);
+        
+        Player player = new Player(200,50, gameStage);
+
+    }
+
+    /* (non-Javadoc)
+     * @see com.github.lhrb.nemo.screen.AbstractScreen#update(float)
+     */
+    @Override
+    public void update(float delta) {
+        CollisionManager.checkCollision(getPhysicalActors());
+        // TODO Auto-generated method stub
+    }
+}
