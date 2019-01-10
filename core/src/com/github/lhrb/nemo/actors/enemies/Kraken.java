032b5ec03e9dddf13d18d994561a6d80ec22d4e0
Christoph Waninger <christoph@cwsbit.de>
2019-01-10 17:16:25 +0100

    fix kraken section score values in init

--- core/src/com/github/lhrb/nemo/actors/enemies/Kraken.java
+++ core/src/com/github/lhrb/nemo/actors/enemies/Kraken.java
@@ -40,19 +40,19 @@ public class Kraken extends MultiPartActor {
     }
     
     private void init() {
-        addPart(new Section(0, this, 75,50, 15, 500,
+        addPart(new Section(0, this, 75,50, 15, 200,
                     AnimationLoader.get().texture("krake_kopf.png")) );
-        addPart(new Section(1, this, 30,100, 5, 50,
+        addPart(new Section(1, this, 30,100, 5, 100,
                 AnimationLoader.get().texture("krake_flosse1.png")) );
-        addPart(new Section(2, this, 0,50, 5, 50,
+        addPart(new Section(2, this, 0,50, 5, 100,
                 AnimationLoader.get().texture("krake_flosse2.png")) );
-        addPart(new Section(3, this, 60,0, 5, 50,
+        addPart(new Section(3, this, 60,0, 5, 100,
                 AnimationLoader.get().texture("krake_flosse3.png")) );
-        addPart(new Section(4, this, 140,0, 5, 50,
+        addPart(new Section(4, this, 140,0, 5, 100,
                 AnimationLoader.get().texture("krake_flosse4.png")) );
-        addPart(new Section(5, this, 170,50, 5, 50,
+        addPart(new Section(5, this, 170,50, 5, 100,
                 AnimationLoader.get().texture("krake_flosse5.png")) );
-        addPart(new Section(6, this, 170,100, 5, 50,
+        addPart(new Section(6, this, 170,100, 5, 100,
                 AnimationLoader.get().texture("krake_flosse6.png")) );
         
         setRotation(0);
